8c3761257aa968adbd0abe399473fce95b7708aa
Nurislam <maksatbekkov@icloud.com>
2023-06-13 18:44:22 +0600

    hw has done!

--- src/Service.java
+++ src/Service.java
@@ -4,59 +4,71 @@ import java.util.*;
 
 public class Service {
 
-    private  Map<Socket, Integer> storage = new HashMap<>();
+    private Map<Socket, Integer> storage = new HashMap<>();
     private int number;
 
-    public  void handle(Socket socket) {
+    public void handle(Socket socket) throws IOException {
         setNumber(getNumber() + 1);
-        System.out.printf("Connected new user: %s%n,", socket);
+        System.out.printf("Connected new user: %s%n", socket);
         storage.put(socket, getNumber());
+
         try (socket;
              var reader = getReader(socket);
-             PrintWriter writer = getWriter(socket)){
+             PrintWriter writer = getWriter(socket)) {
+
             sendResponse("Your number is " + storage.get(socket), writer);
-            storage.put(socket, getNumber());
-            while (true){
+
+            while (true) {
                 var message = reader.nextLine().strip();
-                for (Socket connection: storage.keySet()) {
-                    if(!Objects.equals(connection, storage) && !isEmptyMsg(message)){
-                        sendResponse(storage.get(socket) + ") " + message, getWriter(connection)); }
-                }
-                if(isQuiteMsg(message)){
-                    System.out.printf("%s left the chat", getNumber());
+                if (isQuiteMsg(message)) {
+                    for (Socket conn: storage.keySet()){
+                        System.out.printf("User %s left the chat%n", storage.get(socket));
+                        sendResponse("User " + storage.get(socket) + ") left the chat ", getWriter(conn));
+                    }
                     storage.remove(socket);
-                }else if(isEmptyMsg(message)){
-                    sendResponse("You have to type something to send a message!", getWriter(socket));
+                    break;
+                } else if (!isEmptyMsg(message)) {
+                    for (Socket connection : storage.keySet()) {
+                        if (!Objects.equals(connection, socket)) {
+                            sendResponse("User " + storage.get(socket) + ") " + message, getWriter(connection));
+                        }
+                    }
+                } else {
+                    sendResponse("You have to type something to send a message!", writer);
                 }
             }
-        }catch (NoSuchElementException e) {
-            System.out.println("Client dropped connection");
+        } catch (NoSuchElementException e) {
+            for (Socket conn: storage.keySet()){
+                System.out.printf("User %s left the chat%n", storage.get(socket));
+                if(conn != socket){
+                sendResponse("User " + storage.get(socket) + ") left the chat ", getWriter(conn));}
+            }
             storage.remove(socket);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private PrintWriter getWriter(Socket socket) throws IOException{
+    private PrintWriter getWriter(Socket socket) throws IOException {
         OutputStream out = socket.getOutputStream();
         return new PrintWriter(out);
     }
 
-    private Scanner getReader(Socket socket) throws IOException{
+    private Scanner getReader(Socket socket) throws IOException {
         InputStream input = socket.getInputStream();
         InputStreamReader reader = new InputStreamReader(input, "UTF-8");
         return new Scanner(reader);
     }
 
-    private Boolean isQuiteMsg(String msg){
+    private Boolean isQuiteMsg(String msg) {
         return "bye".equalsIgnoreCase(msg);
     }
 
-    private Boolean isEmptyMsg(String msg){
+    private Boolean isEmptyMsg(String msg) {
         return msg == null || msg.isBlank();
     }
 
-    private void sendResponse(String response, Writer writer) throws IOException{
+    private void sendResponse(String response, Writer writer) throws IOException {
         writer.write(response);
         writer.write(System.lineSeparator());
         writer.flush();
@@ -70,3 +82,4 @@ public class Service {
         this.number = number;
     }
 }
+
