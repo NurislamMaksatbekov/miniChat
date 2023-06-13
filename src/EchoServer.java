19aaeebcc5bf6bab7e00b0d4108199facc406373
Nurislam <maksatbekkov@icloud.com>
2023-06-13 16:44:26 +0600

    i done function to send messages

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,18 +1,18 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EchoServer {
     private final int port;
     private final ExecutorService pool = Executors.newCachedThreadPool();
+    private final Service service = new Service();
 
     public EchoServer(int port) {
         this.port = port;
     }
+
 
     public static EchoServer bindToPort(int port){
         return new EchoServer(port);
@@ -22,7 +22,7 @@ public class EchoServer {
         try(var server = new ServerSocket(port)){
             while (!server.isClosed()){
                 Socket clientSocket = server.accept();
-                pool.submit(() -> handle(clientSocket));
+                pool.submit(() -> service.handle(clientSocket));
             }
         }catch (IOException e){
             System.out.printf("Вероятнее всего порт %s занят.%n", port);
@@ -30,49 +30,5 @@ public class EchoServer {
         }
     }
 
-    private void handle(Socket socket) {
-        System.out.printf("Connected new user: %s%n,", socket);
-        try (socket;
-             var reader = getReader(socket);
-             PrintWriter writer =getWriter(socket)){
-            while (true){
-                var message = reader.nextLine().strip();
-                System.out.printf("Got: %s%n", message);
-                if(isEmptyMsg(message) || isQuiteMsg(message)){
-                    break;
-                }
-                sendResponse(message, writer);
-            }
-        }catch (NoSuchElementException e) {
-            System.out.println("Client dropped connection");
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
 
-    private PrintWriter getWriter(Socket socket) throws IOException{
-        OutputStream out = socket.getOutputStream();
-        return new PrintWriter(out);
-    }
-
-    private Scanner getReader(Socket socket) throws IOException{
-        InputStream input = socket.getInputStream();
-        InputStreamReader reader = new InputStreamReader(input, "UTF-8");
-        return new Scanner(reader);
-    }
-
-    private Boolean isQuiteMsg(String msg){
-        return "bye".equalsIgnoreCase(msg);
-    }
-
-    private Boolean isEmptyMsg(String msg){
-        return msg == null || msg.isBlank();
-    }
-
-    private void sendResponse(String response, Writer writer) throws IOException{
-        writer.write(response);
-        writer.write(System.lineSeparator());
-        writer.flush();
-
-    }
 }
--- src/Service.java
+++ src/Service.java
@@ -1,3 +1,71 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Service {
 
+    private  Map<Socket, Integer> storage = new HashMap<>();
+    private int number;
+
+    public  void handle(Socket socket) {
+        setNumber(getNumber() + 1);
+        System.out.printf("Connected new user: %s%n,", socket);
+        storage.put(socket, getNumber());
+        try (socket;
+             var reader = getReader(socket);
+             PrintWriter writer = getWriter(socket)){
+            storage.put(socket, getNumber());
+            while (true){
+                var message = reader.nextLine().strip();
+                if(isEmptyMsg(message) || isQuiteMsg(message)){
+                    System.out.printf("%s left the chat", getNumber());
+                }
+                for (Socket connection: storage.keySet()) {
+                    if(connection != storage){
+                        sendResponse(storage.get(socket) + ")" + message, getWriter(connection)); }
+
+                }
+            }
+        }catch (NoSuchElementException e) {
+            System.out.println("Client dropped connection");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private PrintWriter getWriter(Socket socket) throws IOException{
+        OutputStream out = socket.getOutputStream();
+        return new PrintWriter(out);
+    }
+
+    private Scanner getReader(Socket socket) throws IOException{
+        InputStream input = socket.getInputStream();
+        InputStreamReader reader = new InputStreamReader(input, "UTF-8");
+        return new Scanner(reader);
+    }
+
+    private Boolean isQuiteMsg(String msg){
+        return "bye".equalsIgnoreCase(msg);
+    }
+
+    private Boolean isEmptyMsg(String msg){
+        return msg == null || msg.isBlank();
+    }
+
+    private void sendResponse(String response, Writer writer) throws IOException{
+        writer.write(response);
+        writer.write(System.lineSeparator());
+        writer.flush();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
